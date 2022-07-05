10ef6cfea1a69b891bc715da6ce2a664fc794410
Dima Kolesnyk <kolesnyk.dmytro.victorovych@gmail.com>
2022-07-05 23:37:38 +0300

    First commit

---
+++ for-stars.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let result = '';
+
+
+for (let i = 1; i < 7; i++){
+    for (let j = 0; j < i; j++){
+        result += '*';
+    }
+    result +='\n';
+}
+console.log(result);
+
+
+
